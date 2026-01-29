cdc7b96d14a1aae2f28d5d36e1d87b7637744a9d
Omkar Kamat <mail4omkarkamat@gmail.com>
2026-01-30 01:43:54 +0530

    delete

--- journal/src/main/java/com/learn/journal/repo/JournalRepo.java
+++
@@ -1,9 +0,0 @@
-package com.learn.journal.repo;
-
-import com.learn.journal.Entity.JournalEntry;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JournalRepo extends MongoRepository<JournalEntry, ObjectId> {
-
-}
